2258a8972f68ab7773930298540b292319bcb24a
sravankumar1503 <113086819+sravankumar1503@users.noreply.github.com>
2023-01-02 19:32:28 +0530

    Create comments.dart

---
+++ comments.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("single line comments starts with //"); //single line comments
+  print("multiline comments are starting and ending with /*....*/"); 
+  /*void main(){
+  print("hello");
+  }*/
+  print("documentation comments like ///");
+  
+}
+/// hello this is sravan
+  void dohello(){
+    print("if u click on hello method it shows its documentation which we written after ///");
+  }
+
+ 
